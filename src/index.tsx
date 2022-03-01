8d686ba17bb0fc5c7d5bc9f1a0705a54a986ae17
Малякин Кирилл <kirill.malyakin@gazprombank.ru>
2022-03-01 14:05:52 +0300

    feat: добавил экспорт FlexInline

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 export { Content } from "./atoms/Content";
+export { FlexInline } from "./atoms/FlexInline";
 export { LoadingContainer } from "./atoms/LoadingContainer";
 export { FadeDown } from "./atoms/animations/FadeDown";
 
