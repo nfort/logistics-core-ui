614049eaa71a2a4562862ca4973da5d62cad8630
Kirill Malyakin <kirillmalyakin@MacBook-Air-Kirill.local>
2022-07-21 16:54:49 +0300

    perf: обернул функции в useCallback
    BREAKING CHANGE: Теоретический это может сломать старый код написанный с этими хуками

--- src/hooks/formik/useArrayFieldFormik.ts
+++ src/hooks/formik/useArrayFieldFormik.ts
@@ -1,4 +1,5 @@
 import { useFormikContext } from "formik";
+import { useCallback } from "react";
 
 export function useArrayFieldFormik<Values extends { [key: string]: string[] }>(name: string) {
   const { values, setFieldValue, setFieldTouched } = useFormikContext<Values>();
@@ -10,16 +11,19 @@ export function useArrayFieldFormik<Values extends { [key: string]: string[] }>(
     throw new Error("Field in formik doesn't exist");
   }
 
-  const updater = (newValue: string) => {
-    const copyValue = [...value];
-    if (copyValue.includes(newValue)) {
-      copyValue.splice(copyValue.indexOf(newValue));
-    } else {
-      copyValue.push(newValue);
-    }
-    setFieldValue(name, copyValue, false);
-    setFieldTouched(name, true, false);
-  };
+  const updater = useCallback(
+    (newValue: string) => {
+      const copyValue = [...value];
+      if (copyValue.includes(newValue)) {
+        copyValue.splice(copyValue.indexOf(newValue));
+      } else {
+        copyValue.push(newValue);
+      }
+      setFieldValue(name, copyValue, false);
+      setFieldTouched(name, true, false);
+    },
+    [setFieldValue, setFieldTouched]
+  );
 
   return [value, updater] as const;
 }
--- src/hooks/formik/useDeleteFieldFormik.ts
+++ src/hooks/formik/useDeleteFieldFormik.ts
@@ -1,26 +1,30 @@
 import { useFormikContext } from "formik";
 import cloneDeep from "lodash.clonedeep";
+import { useCallback } from "react";
 
 export function useDeleteFieldFormik() {
   const { setFormikState } = useFormikContext<{ [key: string]: string | number | string[] | number[] }>();
 
-  const remove = (fieldOrFields: string | string[]) => {
-    setFormikState((state) => {
-      const newState = cloneDeep(state);
-      if (Array.isArray(fieldOrFields)) {
-        fieldOrFields.forEach((field) => {
-          delete newState.values[field];
-          delete newState.touched[field];
-          delete newState.errors[field];
-        });
-      } else {
-        delete newState.values[fieldOrFields];
-        delete newState.touched[fieldOrFields];
-        delete newState.errors[fieldOrFields];
-      }
-      return newState;
-    });
-  };
+  const remove = useCallback(
+    (fieldOrFields: string | string[]) => {
+      setFormikState((state) => {
+        const newState = cloneDeep(state);
+        if (Array.isArray(fieldOrFields)) {
+          fieldOrFields.forEach((field) => {
+            delete newState.values[field];
+            delete newState.touched[field];
+            delete newState.errors[field];
+          });
+        } else {
+          delete newState.values[fieldOrFields];
+          delete newState.touched[fieldOrFields];
+          delete newState.errors[fieldOrFields];
+        }
+        return newState;
+      });
+    },
+    [setFormikState]
+  );
 
   return remove;
 }
