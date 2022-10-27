40f8a2fb845dcd2ab7e9071d11bc95fbf4668cd1
Kirill Malyakin <kirillmalyakin@MacBook-Air-Kirill.local>
2022-10-27 16:24:01 +0300

    feat: добавил вывод кол-во фильтров в Filter

--- src/components/Table/components/Filter.tsx
+++ src/components/Table/components/Filter.tsx
@@ -19,6 +19,12 @@ export function Filter({ globalFilter, setGlobalFilter, columns, children }: Pro
     setGlobalFilter(value || undefined);
   }, 200);
   const hasFilters = columns.some((column) => !column.disableFilters);
+  const amountFilters = columns.reduce((acc, f) => {
+    if (f.filterValue) {
+      acc++;
+    }
+    return acc;
+  }, 0);
 
   return (
     <Styled>
@@ -38,7 +44,12 @@ export function Filter({ globalFilter, setGlobalFilter, columns, children }: Pro
         </div>
         <div>
           {children}
-          <FiltersControlButton hasFilter={hasFilters} opened={showAllFilters} onToggle={toggleAllFilters} />
+          <FiltersControlButton
+            hasFilter={hasFilters}
+            opened={showAllFilters}
+            onToggle={toggleAllFilters}
+            amountFilters={amountFilters}
+          />
         </div>
       </div>
       <FadeDown in={showAllFilters}>
@@ -59,20 +70,40 @@ function FiltersControlButton({
   onToggle,
   hasFilter,
   opened,
+  amountFilters,
 }: {
   hasFilter: boolean;
   opened: ReturnedTypeUseToggle[0];
   onToggle: ReturnedTypeUseToggle[1];
+  amountFilters: number;
 }) {
   if (hasFilter) {
     return (
-      <Button type="button" isWithIcon={true} color="tertiary" onClick={onToggle} dimension="medium">
-        {opened ? <ArrowUpIcon size={20} /> : <PropertiesIcon size={20} />}
-      </Button>
+      <div style={{ position: "relative" }}>
+        <Button type="button" isWithIcon={true} color="tertiary" onClick={onToggle} dimension="medium">
+          {opened ? <ArrowUpIcon size={20} /> : <PropertiesIcon size={20} />}
+        </Button>
+        {!opened && amountFilters ? <AmountFilters>{Math.min(amountFilters, 9)}</AmountFilters> : null}
+      </div>
     );
   }
   return null;
 }
+
+const AmountFilters = styled.div`
+  position: absolute;
+  top: -9px;
+  right: -9px;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  padding: 2px;
+
+  background-color: rgb(227, 229, 236);
+  color: rgb(38, 44, 64);
+  font-size: 14px;
+  text-align: center;
+`;
 
 const Styled = styled.div`
   margin-bottom: 2rem;
